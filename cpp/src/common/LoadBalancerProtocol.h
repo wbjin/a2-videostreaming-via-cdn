b9443f3c84f017d12d28deae28678732ccf7e64c
aditya singhvi <singhvi@umich.edu>
2025-01-28 03:43:01 -0500

    update cpp structure and cmake files
    Former-commit-id: 3ce5e9df5ae4e0748f32eb1530a975014501560b

--- cpp/src/common/LoadBalancerProtocol.h
+++ cpp/src/common/LoadBalancerProtocol.h
@@ -1,5 +1,23 @@
+#include <cstdint>
+#include <netinet/in.h>
+
+// YOU SHOULD NOT MODIFY THIS FILE --------------------------------------------
+
+// Define the protocol between the load balancer and the adaptive proxy.
+
+// The adaptive proxy sends a request to the load balancer containing the IP
+// address of the client it wishes to serve.
+
+// The load balancer replies with the (address, port) of the videoserver that
+// the adaptive proxy should use for that particular client.
 
 struct LoadBalancerRequest {
-    
+    in_addr_t client_addr; // The IP address of the client.
+    uint16_t request_id;   // A randomly-chosen identifier for this request.
+} __attribute__((__packed__));
 
-}
+struct LoadBalancerResponse {
+    in_addr_t videoserver_addr; // The IP address of the videoserver.
+    uint16_t videoserver_port;  // The port of the videoserver.
+    uint16_t request_id;        // The request_id from the LoadBalancerRequest.
+} __attribute__((__packed__));
