5ba24de1bfc4d004a441d767c628db694c8b8b3e
aditya singhvi <singhvi@umich.edu>
2025-02-02 01:12:21 -0500

    Add rand note
    Former-commit-id: 397f84294f08c703dc089e918a0058256415d375

--- cpp/src/common/LoadBalancerProtocol.h
+++ cpp/src/common/LoadBalancerProtocol.h
@@ -18,6 +18,17 @@ struct LoadBalancerRequest {
     uint16_t request_id;   // A randomly-chosen identifier for this request.
 };
 
+/*
+ * Note about "randomly-chosen identifier":
+ *
+ * In C++, it is tempting to use the rand() function to generate a random
+ * number. However, this function is not used in modern code because of its many
+ * shortcomings. For more information, see
+ * https://stackoverflow.com/questions/53040940/why-is-the-new-random-library-better-than-stdrand
+ * For this project, you are free to use it if you would like, but you should
+ * feel a little guilty about it while doing so.
+ */
+
 struct LoadBalancerResponse {
     in_addr_t videoserver_addr; // The IP address of the videoserver.
     uint16_t videoserver_port;  // The port of the videoserver.
